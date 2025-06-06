09f9162696b8e1fd8209974d44f894ea86886408
Vivek Chavda <115214436+Vivekchavda1374@users.noreply.github.com>
2025-06-06 20:23:58 +0530

    all code added

--- Mode.java
+++ Mode.java
@@ -19,8 +19,28 @@ public class Mode {
 
     }
     public static int[] findModes(int[] arr) {
-
+        HashMap<Integer, Integer> frequencyMap = new HashMap<>();
+        int maxFrequency = 0;
+        for (int num : arr) {
+            int freq = frequencyMap.getOrDefault(num, 0) + 1;
+            frequencyMap.put(num, freq);
+            if (freq > maxFrequency) {
+                maxFrequency = freq;
+            }
+        }
+        List<Integer> modesList = new ArrayList<>();
+        for (int num : frequencyMap.keySet()) {
+            if (frequencyMap.get(num) == maxFrequency) {
+                modesList.add(num);
+            }
+        }
+        int[] modes = new int[modesList.size()];
+        for (int i = 0; i < modesList.size(); i++) {
+            modes[i] = modesList.get(i);
+        }
+        return modes;
     }
+
 
 public static void main(String[] args) {
     int[] arr = {1, 2, 3, 3, 4, 4, 5};
